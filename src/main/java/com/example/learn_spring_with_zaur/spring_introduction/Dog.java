3fcf03aad561bbd8f3a0ed8b5a54d65dddc0740d
jaksil1k <nartai53@gmail.com>
2022-08-27 12:35:26 +0600

    I found out difference between singleton and prototype in beans

--- src/main/java/com/example/learn_spring_with_zaur/spring_introduction/Dog.java
+++ src/main/java/com/example/learn_spring_with_zaur/spring_introduction/Dog.java
@@ -1,8 +1,18 @@
 package com.example.learn_spring_with_zaur.spring_introduction;
 
 public class Dog implements Pet{
+    private String name;
+
     @Override
     public void say(){
         System.out.println("Bow-Wow");
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
     }
 }
---
+++ src/main/java/com/example/learn_spring_with_zaur/spring_introduction/Test4.java
@@ -0,0 +1,24 @@
+package com.example.learn_spring_with_zaur.spring_introduction;
+
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+public class Test4 {
+    public static void main(String[] args){
+        ClassPathXmlApplicationContext context = new
+                ClassPathXmlApplicationContext("applicationContext2.xml");
+
+        Dog myDog = context.getBean("myPet", Dog.class);
+        myDog.setName("Belka");
+        Dog yourDog = context.getBean("myPet", Dog.class);
+        yourDog.setName("Strelka");
+
+        System.out.println(myDog.getName());
+        System.out.println(yourDog.getName());
+        // when scope of bean is singleton the answer will be: Strelka Strelka
+        //when scope is prototype, the answer will be: Belka Strelka
+        /*
+        Singleton create object when maven reads config file
+        Prototype create object when we call method getBean
+         */
+    }
+}
