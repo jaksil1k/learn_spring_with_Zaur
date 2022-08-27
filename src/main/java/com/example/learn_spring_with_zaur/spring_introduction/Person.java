8c0ec87b0774f91fba9554456931b7ba0292ccaf
jaksil1k <nartai53@gmail.com>
2022-08-27 20:02:33 +0600

    Autowired in setter and field

--- src/main/java/com/example/learn_spring_with_zaur/spring_introduction/Person.java
+++ src/main/java/com/example/learn_spring_with_zaur/spring_introduction/Person.java
@@ -5,15 +5,16 @@ import org.springframework.stereotype.Component;
 
 @Component("personBean")
 public class Person {
+    @Autowired
     private Pet pet;
     private String lastName;
     private int age;
 
-    @Autowired
-    public Person(Pet pet) {
-        System.out.println("Person has been created");
-        this.pet = pet;
-    }
+//    @Autowired
+//    public Person(Pet pet) {
+//        System.out.println("Person has been created");
+//        this.pet = pet;
+//    }
 
 
     public void callYourPet(){
@@ -21,10 +22,11 @@ public class Person {
         pet.say();
     }
 
-    public void setPet(Pet pet){
-        System.out.println("Class Person: set pet");
-        this.pet = pet;
-    }
+//    @Autowired
+//    public void setPet(Pet pet){
+//        System.out.println("Class Person: set pet");
+//        this.pet = pet;
+//    }
 
     public String getLastName() {
         return lastName;
